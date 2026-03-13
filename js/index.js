47c02da495da0882a410556917910e9218b20260
sau <astezkina03@gmail.com>
2026-03-14 01:57:19 +0700

    Графическое отображение строк

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ const CONFIG = {
         starts: '#category-starts',
         operator: '#category-operators',
         variable: '#category-variables',
+        string: '#category-string',
         event: '#category-events'
     },
     DEFAULT_POS: { left: 60, top: 60 },
@@ -957,11 +958,13 @@ class SlidingMenu {
         this.buttons = {
             starts: Utils.$('#button-tag-starts'),
             variables: Utils.$('#button-tag-variables'),
+            string: Utils.$('#button-tag-string'),
             operators: Utils.$('#button-tag-operators')
         };
         this.cats = {
             starts: Utils.$('#category-starts'),
             variables: Utils.$('#category-variables'),
+            string: Utils.$('#category-string'),
             operators: Utils.$('#category-operators')
         };
         this.activeBtn = null;
